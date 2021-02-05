df8e05b6e1f7a6712f83957c8b7b6295f847fb71
Lea Weissman <lea.weissman@icloud.com>
2021-02-05 10:36:49 +0100

    bla

--- src/app.js
+++ src/app.js
@@ -2,3 +2,17 @@ import { toggleAnswer } from './toggle';
 
 const buttons = document.querySelectorAll('.card__button')
 buttons.forEach(toggleAnswer);
+
+
+
+const bookmarks = document.querySelector('.bookmark__saved')
+
+function bookmarks(currywurst) {
+
+    currywurst.addEventListener('click', () => {
+        const bookmark = currywurst.parentNode.querySelector('.bookmark__saved');
+        answer.classList.toggle('fas');
+        answer.classList.toggle('far');
+
+    });
+}
