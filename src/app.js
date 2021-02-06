94dd86aa3a7a5a8c35719217507386d6c4efb6c4
Lea Weissman <lea.weissman@icloud.com>
2021-02-06 12:43:36 +0100

    added bookmark toggle

--- src/app.js
+++ src/app.js
@@ -5,14 +5,12 @@ buttons.forEach(toggleAnswer);
 
 
 
-const bookmarks = document.querySelector('.bookmark__saved')
+const allbookmarks = document.querySelectorAll('.fa-bookmark')
 
-function bookmarks(currywurst) {
-
-    currywurst.addEventListener('click', () => {
-        const bookmark = currywurst.parentNode.querySelector('.bookmark__saved');
-        answer.classList.toggle('fas');
-        answer.classList.toggle('far');
-
-    });
+allbookmarks.forEach((singlebookmark) => {
+    singlebookmark.addEventListener('click', () => {
+        singlebookmark.classList.toggle('far');
+        singlebookmark.classList.toggle('fas');
+    })
 }
+)
